b48614f81f2e6c281cb1f6cb3306420f66afe7a6
DESKTOP-CMOH7M4\blueg <blueg0826@gmail.com>
2020-02-18 03:32:34 +0900

    addGameEvent

---
+++ MotivationController/Assets/ScriptableTest/GameEvent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "MyGame/Create GameEvent", fileName = "GameEvent")]
+public class GameEvent : ScriptableObject
+{
+    private List<GameEventListener> eventistenrs = new List<GameEventListener>();
+
+    public void Raise()
+    {
+        for(int i = 0; i < eventistenrs.Count; i++)
+        {
+            eventistenrs[i].OnEventRaised();
+        }
+    }
+
+    public void RegisterListener(GameEventListener listener)
+    {
+        if (!eventistenrs.Contains(listener))
+            eventistenrs.Add(listener);
+    }
+
+    public void UnregisterListener(GameEventListener listener)
+    {
+        eventistenrs.Remove(listener);
+    }
+}
